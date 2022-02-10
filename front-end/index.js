779cf82a4d57d6f465cc06e3d2cf9ff72447b6f9
elmehdi rhindi <g6handi@gmail.com>
2022-02-10 20:39:38 +0100

    create course

--- front-end/index.js
+++ front-end/index.js
@@ -1,16 +1,32 @@
 const readAllBtn = document.querySelector(".readAllBtn");
 const readOneBtn = document.querySelector(".readOneBtn");
+const createBtn = document.querySelector(".createBtn");
+
+const inputId = document.querySelector(".inputId");
+const inputName = document.querySelector(".inputName");
+const inputType = document.querySelector(".inputType");
 const list = document.querySelector(".list");
+const oneCourse = document.querySelector(".oneCourse");
+const oneNewCourse = document.querySelector(".oneNewCourse");
 
 readAllBtn.addEventListener("click", async () => {
   const coursesResult = await axios.get("http://localhost:8001/courses");
   const listElements = coursesResult.data
     .map((course) => `<li>${course.name}</li>`)
     .reduce((prev, curr) => prev + curr);
-  list.innerHTML = listElements
+  list.innerHTML = listElements;
 });
 
-readOneBtn.addEventListener('click', async () => {
-  const courseResult = await axios.get("http://localhost:8001/courses/2")
-  console.log(courseResult.data)
-})
+readOneBtn.addEventListener("click", async () => {
+  const courseResult = await axios.get(
+    `http://localhost:8001/courses/${inputId.value}`
+  );
+  oneCourse.innerHTML = courseResult.data.name;
+});
+
+createBtn.addEventListener("click", async () => {
+  await axios.post("http://localhost:8001/courses", {
+    name: inputName.value,
+    type: inputType.value,
+  });
+});
